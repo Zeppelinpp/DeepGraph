744e5a7456f4f9d400378d965d3a53b854054432
Zeppelinpp <1056118649@qq.com>
2025-09-22 15:43:56 +0800

    Simple k,v retriever

--- src/utils/retriever.py
+++ src/utils/retriever.py
@@ -1,7 +1,18 @@
+import orjson
+
 class KnowledgeRetriever:
     def __init__(self, persist_directory: str):
         self.persist_directory = persist_directory
         
 
     def retrieve(self, query: str):
-        pass
+        with open(self.persist_directory, "r") as f:
+            data = orjson.loads(f.read())
+        if query in data:
+            return data[query]["framework"]
+        else:
+            return "暂无相关知识"
+
+if __name__ == "__main__":
+    retriever = KnowledgeRetriever(persist_directory="config/analysis_frame.json")
+    print(retriever.retrieve("费用分析"))
