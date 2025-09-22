d063b7cc746ab366b5f7b99128c08a024d6819c0
Zeppelinpp <1056118649@qq.com>
2025-09-22 15:43:13 +0800

    Adjust Task, TaskList structure

--- src/models/base.py
+++ src/models/base.py
@@ -1,12 +1,22 @@
 from pydantic import BaseModel
-from typing import Literal, List
+from typing import Literal, List, Optional
 
 class Task(BaseModel):
-    task_name: str
-    task_description: str
-    task_result: str
-    task_status: Literal["pending", "in_progress", "completed", "failed"]
-    success: bool
+    name: str
+    description: str
+    result: Optional[str] = None
+    status: Optional[Literal["pending", "in_progress", "completed", "failed"]] = None
+    success: Optional[bool] = None
 
 class TaskList(BaseModel):
-    tasks: List[Task]
+    sequential_tasks: List[Task]
+    parallel_tasks: List[Task]
+
+
+if __name__ == "__main__":
+    task = {
+        "name": "任务名称",
+        "description": "任务描述"
+    }
+    task_model = Task.model_validate(task)
+    print(task_model)
